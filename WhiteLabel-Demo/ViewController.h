7d8d876ab6a36f9b65f49bf422422ed9ff6ee7d1
mha-fueled <mha@fueled.co>
2014-10-18 12:08:32 +0530

    Initial Commit

---
+++ WhiteLabel-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhiteLabel-Demo
+//
+//  Created by Ahuja on 18/10/14.
+//  Copyright (c) 2014 Fueled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WhiteLabel-Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WhiteLabel-Demo
+//
+//  Created by Ahuja on 18/10/14.
+//  Copyright (c) 2014 Fueled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
