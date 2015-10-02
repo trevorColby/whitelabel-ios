fef4aede74300b9062c6544b27e3c58aebf98a46
stephane-fueled <stephane@fueled.com>
2015-10-02 13:37:02 -0400

    tests(library/register+login): add tests for login & register

--- WhiteLabelChatTests/WhiteLabelChatTests.swift
+++ WhiteLabelChatTests/WhiteLabelChatTests.swift
@@ -9,26 +9,106 @@
 import XCTest
 @testable import WhiteLabelChat
 
+private let APICallTimeout = 30.0
+
+var currentUser: User!
+
 class WhiteLabelChatTests: XCTestCase {
+	private let username = "unittest"
+	private let password = "unittest"
+	
+	private var chatController: ChatController!
+	
 	override func setUp() {
-		super.setUp()
-		// Put setup code here. This method is called before the invocation of each test method in the class.
+		Configuration.defaultBaseURL = NSURL(string: "http://172.16.30.159:3000/")
+		
+		self.chatController = ChatController()
 	}
 	
 	override func tearDown() {
-		// Put teardown code here. This method is called after the invocation of each test method in the class.
-		super.tearDown()
+		self.chatController = nil
 	}
 	
-	func testExample() {
-		// This is an example of a functional test case.
-		// Use XCTAssert and related functions to verify your tests produce the correct results.
+	func testAAAAA_CreateOrLoginUnitTestAccount() {
+		var finished = false
+		
+		User.registerWithUsername(self.username, password: self.password) { (user, error) -> () in
+			if let user = user {
+				currentUser = user
+				
+				finished = true
+			} else {
+				User.loginWithUsername(self.username, password: self.password) { (user, error) -> () in
+					currentUser = user
+					
+					finished = true
+				}
+			}
+		}
+		
+		while !finished {
+			NSRunLoop.currentRunLoop().runMode(NSDefaultRunLoopMode, beforeDate: NSDate.distantFuture())
+		}
+		
+		XCTAssertNotNil(currentUser.authToken)
+		
+		if let authToken = currentUser.authToken {
+			print("Current authenticated token: \(authToken)")
+		}
 	}
 	
-	func testPerformanceExample() {
-		// This is an example of a performance test case.
-		self.measureBlock {
-			// Put the code you want to measure the time of here.
+	func testUserSignUp() {
+		let username = "test\(arc4random())"
+		let password = "test\(arc4random())"
+		let expectation = expectationWithDescription("Register & Login successfully")
+		User.registerWithUsername(username, password: password) { (user, error) -> () in
+			XCTAssertNotNil(user)
+			XCTAssertNil(error)
+			guard let user = user else {
+				return
+			}
+			XCTAssertEqual(user.username, username)
+			XCTAssertTrue(user.isAuthenticated)
+			
+			User.loginWithUsername(username, password: password) { (user, error) -> () in
+				expectation.fulfill()
+				
+				XCTAssertNotNil(user)
+				XCTAssertNil(error)
+				
+				guard let user = user else {
+					return
+				}
+				
+				XCTAssertEqual(user.username, username)
+				XCTAssertTrue(user.isAuthenticated)
+			}
+		}
+		waitForExpectationsWithTimeout(APICallTimeout) { (error) -> Void in
+			if let error = error {
+				print(error)
+			}
+		}
+	}
+	
+	func testUserInvalidRequestLogin() {
+		let expectation = expectationWithDescription("Login fails because of a validation error")
+		User.loginWithUsername("*&$^#!", password: "fueled") { (user, error) -> () in
+			XCTAssertNil(user)
+			XCTAssertNotNil(error)
+			switch(error!) {
+			case ErrorCode.RequestFailed:
+				break
+			default:
+				XCTFail("error is not \(ErrorCode.RequestFailed)")
+			}
+			
+			expectation.fulfill()
+		}
+		waitForExpectationsWithTimeout(APICallTimeout) { (error) -> Void in
+			if let error = error {
+				print(error)
+			}
 		}
 	}
 }
